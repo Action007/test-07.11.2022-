edcb1495c30fbd988947ec11ab8d549992eb541f
Action007 <aqshin.baghirzade@gmail.com>
2022-06-24 11:35:29 +0400

    CCM-284

--- src/components/components/ActiveChecklistDetail/ActiveChecklistDetail.jsx
+++ src/components/components/ActiveChecklistDetail/ActiveChecklistDetail.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import ChecklistCheckbox from "../ChecklistCheckbox/ChecklistCheckbox";
-import PopupCreateDone from "../PopupCreateDone/PopupCreateDone";
 import uniqueID from "../../../utils/uniqueID";
 import "./ActiveChecklistDetail.scss";
 
@@ -9,54 +9,47 @@ import { ReactComponent as UploadSvg } from "../../../assets/images/icon/upload.
 import { ReactComponent as ShareSvg } from "../../../assets/images/icon/share.svg";
 
 const ActiveChecklistDetail = ({ checklist }) => {
-  const [checklistItems, setChecklistItems] = useState(checklist);
-  const [modalShow, setModalShow] = useState(checklist.completed);
-
-  useEffect(() => {
-    if (checklist.completed) {
-      setModalShow(checklist.completed);
-    }
-  }, [checklist.completed]);
+  const [checklistItems, setChecklistItems] = useState(
+    checklist.checklist_items
+  );
+  const { t: translate } = useTranslation();
 
   return (
-    <>
-      <div className="active-checklist">
-        <h3 className="active-checklist__title SFPro-700">{checklist.name}</h3>
-        <ol className="active-checklist__items">
-          {checklistItems.checklist_items.map(
-            ({ description, list_type, value, completed, id }) => {
-              const idFor = uniqueID();
-              return (
-                <ChecklistCheckbox
-                  key={id}
-                  id={checklist.id}
-                  description={description}
-                  checklistItemId={id}
-                  list_type={list_type}
-                  value={value}
-                  idFor={idFor}
-                  completed={completed}
-                  setChecklistItems={setChecklistItems}
-                />
-              );
-            }
-          )}
-        </ol>
-        <div className="active-checklist__wrapper SFPro-500">
-          <button className="active-checklist__btn" type="button">
-            Share
-            <ShareSvg />
-          </button>
-          <button className="active-checklist__btn" type="button">
-            <UploadSvg />
-          </button>
-          <button className="active-checklist__btn" type="button">
-            <DeleteSvg />
-          </button>
-        </div>
+    <div className="active-checklist">
+      <h3 className="active-checklist__title SFPro-700">{checklist.name}</h3>
+      <ol className="active-checklist__items">
+        {checklistItems.map(
+          ({ description, list_type, value, completed, id }) => {
+            const idFor = uniqueID();
+            return (
+              <ChecklistCheckbox
+                key={id}
+                id={checklist.id}
+                description={description}
+                checklistItemId={id}
+                list_type={list_type}
+                value={value}
+                idFor={idFor}
+                completed={completed}
+                setChecklistItems={setChecklistItems}
+              />
+            );
+          }
+        )}
+      </ol>
+      <div className="active-checklist__wrapper SFPro-500">
+        <button className="active-checklist__btn" type="button">
+          {translate("share")}
+          <ShareSvg />
+        </button>
+        <button className="active-checklist__btn" type="button">
+          <UploadSvg />
+        </button>
+        <button className="active-checklist__btn" type="button">
+          <DeleteSvg />
+        </button>
       </div>
-      <PopupCreateDone show={modalShow} onHide={() => setModalShow(false)} />
-    </>
+    </div>
   );
 };
 
