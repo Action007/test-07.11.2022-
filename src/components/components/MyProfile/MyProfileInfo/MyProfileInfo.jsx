6836f945c1116040a92bbb0661743059408b8833
Рина Кода <rina4790karina@gmail.com>
2022-06-27 14:04:21 +0000

    Ccm 191

--- src/components/components/MyProfile/MyProfileInfo/MyProfileInfo.jsx
+++ src/components/components/MyProfile/MyProfileInfo/MyProfileInfo.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { checklistAPI } from "../../../../services/checklistService";
 import "./MyProfileInfo.scss";
-
+import LoadingSpinnerPopup from "../../../UI/LoadingSpinnerPopup/LoadingSpinnerPopup";
 import { ReactComponent as Setting } from "../../../../assets/images/icon/setting.svg";
 import { ReactComponent as Facebook } from "../../../../assets/images/icon/facebook.svg";
 import { ReactComponent as Twitter } from "../../../../assets/images/icon/twitter.svg";
@@ -20,109 +22,149 @@ const MyProfileInfo = ({
   facebook,
   twitter,
   instagram,
+  avatar_url,
+  errorSize,
 }) => {
+  const user = useSelector((state) => state.authSliceReducer.user);
   const navigate = useNavigate();
   const { t: translate } = useTranslation();
+  const [editAccount, { isLoading: isUpdateLoading, error }] =
+    checklistAPI.useEditAccountMutation();
+
+  const onLoad = (event) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = () => {
+      const image = reader.result;
+      editAccount({
+        avatar: image,
+      });
+    };
+  };
+
+  useEffect(() => {
+    if (!error) return;
+    errorSize();
+  }, [error]);
 
   return (
-    <div className="profile-info">
-      <div className="profile-info__inner">
-        <div className="profile-info__box">
-          <button className="profile-info__edit-btn" type="button">
-            <div className="profile-info__img">
-              <EmptySvg />
+    <>
+      <LoadingSpinnerPopup showSpinner={isUpdateLoading} />
+      <div className="profile-info">
+        <div className="profile-info__inner">
+          <div className="profile-info__box">
+            <label className="profile-info__edit-btn" htmlFor="uploadImg">
+              <div className="profile-info__img">
+                {avatar_url ? (
+                  <img src={avatar_url} alt="account" />
+                ) : (
+                  <EmptySvg />
+                )}
+              </div>
+              <EditSvg />
+              <input
+                type="file"
+                accept="image/png, image/jpeg, image/jpg"
+                onChange={onLoad}
+                className="profile-info__input"
+                id="uploadImg"
+              />
+            </label>
+            <div>
+              {name && (
+                <h1 className="profile-info__title SFPro-700">{user.name}</h1>
+              )}
+              <span
+                className={`profile-info__subtitle${!country ? " empty" : ""}`}
+              >
+                {country || translate("profilePage.country")}
+              </span>
+              {nickname && (
+                <span className="profile-info__name SFPro-700">
+                  @{nickname}
+                </span>
+              )}
             </div>
-            {/* <img src={ProfileImg} alt="account" /> */}
-            <EditSvg />
-          </button>
-          <div>
-            {name && <h1 className="profile-info__title SFPro-700">{name}</h1>}
-            <span
-              className={`profile-info__subtitle${!country ? " empty" : ""}`}
-            >
-              {country || translate("profilePage.country")}
-            </span>
-            {nickname && (
-              <span className="profile-info__name SFPro-700">@{nickname}</span>
-            )}
           </div>
+          <span
+            className={`profile-info__span${bio.length === 0 ? " empty" : ""}`}
+          >
+            {translate("profilePage.aboutMe")}
+          </span>
+          <p
+            className={`profile-info__text${bio.length === 0 ? " empty" : ""}`}
+          >
+            {bio.length !== 0 ? bio : translate("profilePage.emptyBio")}
+          </p>
+          {facebook || twitter || instagram || website ? (
+            <ul className="profile-info__networks">
+              {facebook && (
+                <li className="profile-info__network">
+                  <a href={facebook} target="_blank" rel="noreferrer">
+                    <Facebook />
+                  </a>
+                </li>
+              )}
+              {twitter && (
+                <li className="profile-info__network">
+                  <a href={twitter} target="_blank" rel="noreferrer">
+                    <Twitter />
+                  </a>
+                </li>
+              )}
+              {instagram && (
+                <li className="profile-info__network">
+                  <a href={instagram} target="_blank" rel="noreferrer">
+                    <Instagram />
+                  </a>
+                </li>
+              )}
+              {website && (
+                <li className="profile-info__network profile-info__network--website">
+                  <a href={website} target="_blank" rel="noreferrer">
+                    <World />
+                    <span className="profile-info__link">
+                      {website.replace(/https?:\/\//g, "")}
+                    </span>
+                  </a>
+                </li>
+              )}
+            </ul>
+          ) : (
+            <ul className="profile-info__networks">
+              <li className="profile-info__network empty">
+                <Facebook />
+              </li>
+              <li className="profile-info__network empty">
+                <Twitter />
+              </li>
+              <li className="profile-info__network empty">
+                <Instagram />
+              </li>
+              <li className="profile-info__network profile-info__network--website empty">
+                <World />
+              </li>
+            </ul>
+          )}
         </div>
-        <span
-          className={`profile-info__span${bio.length === 0 ? " empty" : ""}`}
-        >
-          {translate("profilePage.aboutMe")}
-        </span>
-        <p className={`profile-info__text${bio.length === 0 ? " empty" : ""}`}>
-          {bio.length !== 0 ? bio : translate("profilePage.emptyBio")}
-        </p>
-        {facebook || twitter || instagram || website ? (
-          <ul className="profile-info__networks">
-            {facebook && (
-              <li className="profile-info__network">
-                <a href={facebook} target="_blank" rel="noreferrer">
-                  <Facebook />
-                </a>
-              </li>
-            )}
-            {twitter && (
-              <li className="profile-info__network">
-                <a href={twitter} target="_blank" rel="noreferrer">
-                  <Twitter />
-                </a>
-              </li>
-            )}
-            {instagram && (
-              <li className="profile-info__network">
-                <a href={instagram} target="_blank" rel="noreferrer">
-                  <Instagram />
-                </a>
-              </li>
-            )}
-            {website && (
-              <li className="profile-info__network profile-info__network--website">
-                <a href={website} target="_blank" rel="noreferrer">
-                  <World />
-                  <span className="profile-info__link">
-                    {website.replace(/https?:\/\//g, "")}
-                  </span>
-                </a>
-              </li>
-            )}
-          </ul>
-        ) : (
-          <ul className="profile-info__networks">
-            <li className="profile-info__network empty">
-              <Facebook />
-            </li>
-            <li className="profile-info__network empty">
-              <Twitter />
-            </li>
-            <li className="profile-info__network empty">
-              <Instagram />
-            </li>
-            <li className="profile-info__network profile-info__network--website empty">
-              <World />
-            </li>
-          </ul>
-        )}
+        <div className="profile-info__edit">
+          <button
+            onClick={() => navigate(`/edit-profile`)}
+            className="profile-info__button SFPro-600"
+            type="button"
+          >
+            {translate("profilePage.editProfile")}
+          </button>
+          <button
+            onClick={() => navigate(`/account-settings`)}
+            className="profile-info__setting"
+            type="button"
+          >
+            <Setting />
+          </button>
+        </div>
       </div>
-      <div className="profile-info__edit">
-        <button
-          onClick={() => navigate(`/edit-profile`)}
-          className="profile-info__button SFPro-600"
-          type="button"
-        >
-          {translate("profilePage.editProfile")}
-        </button>
-        <button
-          onClick={() => navigate(`/account-settings`)}
-          className="profile-info__setting"
-          type="button"
-        >
-          <Setting />
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
