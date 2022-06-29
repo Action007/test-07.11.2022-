383d4f8e15a4ca248b51587c38daf90efa85718f
Action007 <aqshin.baghirzade@gmail.com>
2022-06-29 23:42:45 +0400

    CCM-304

--- src/components/components/TermsOfUse/TermsOfUse.jsx
+++ src/components/components/TermsOfUse/TermsOfUse.jsx
@@ -22,9 +22,6 @@ const TermOfUse = () => {
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section1.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section1.subtitle")}
-        </span>
         <p className="term__text SFPro-300 display-7 ">
           {translate("termOfUsePage.section1.text.part1")}
           <a
@@ -46,58 +43,46 @@ const TermOfUse = () => {
         <p className="term__text SFPro-300 display-7">
           {translate("termOfUsePage.section1.text.part5")}
         </p>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section1.text.part6")}
         </p>
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section2.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section2.subtitle")}
-        </span>
         <p className="term__text SFPro-300 display-7 ">
           {translate("termOfUsePage.section2.text.part1")}
         </p>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section2.text.part2")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section3.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section3.subtitle")}
-        </span>
         <p className="term__text SFPro-300 display-7">
           {translate("termOfUsePage.section3.text.part1")}
         </p>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section3.text.part2")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section4.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section4.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section4.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section5.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section5.subtitle")}
-        </span>
         <p className="term__text SFPro-300 display-7 ">
           {translate("termOfUsePage.section5.text.part1")}
         </p>
         <p className="term__text SFPro-300 display-7 ">
           {translate("termOfUsePage.section5.text.part2")}
         </p>
-        <ul className="term__list SFPro-300 display-7 mb-7">
+        <ul className="term__list SFPro-300 display-7 mb-6">
           <li>{translate("termOfUsePage.section5.text.part3")}</li>
           <li>{translate("termOfUsePage.section5.text.part4")}</li>
           <li>{translate("termOfUsePage.section5.text.part5")}</li>
@@ -127,9 +112,6 @@ const TermOfUse = () => {
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section6.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section5.subtitle")}
-        </span>
         <p className="term__text SFPro-300 display-7 ">
           {translate("termOfUsePage.section6.text.part1")}
         </p>
@@ -148,16 +130,13 @@ const TermOfUse = () => {
           <li>{translate("termOfUsePage.section6.text.part13")}</li>
           <li>{translate("termOfUsePage.section6.text.part14")}</li>
         </ul>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section6.text.part15")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section7.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section4.subtitle")}
-        </span>
         <p className="term__text SFPro-300 display-7 ">
           {translate("termOfUsePage.section7.text.part1")}
         </p>
@@ -167,126 +146,93 @@ const TermOfUse = () => {
         <p className="term__text SFPro-300 display-7 ">
           {translate("termOfUsePage.section7.text.part3")}
         </p>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section7.text.part4")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section8.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section8.subtitle")}
-        </span>
         <p className="term__text SFPro-300 display-7 ">
           {translate("termOfUsePage.section8.text.part1")}
         </p>
-        <p className="term__text SFPro-300 display-7 mb-7 ">
+        <p className="term__text SFPro-300 display-7 mb-6 ">
           {translate("termOfUsePage.section8.text.part2")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section9.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section9.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section9.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section10.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section10.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section10.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section11.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section11.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section11.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section12.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section12.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section12.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section13.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section13.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section13.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section14.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section14.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section14.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section15.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section15.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section15.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section16.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section16.subtitle")}
-        </span>
         <p className="term__text SFPro-300 display-7 ">
           {translate("termOfUsePage.section16.text.part1")}
         </p>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section16.text.part2")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section17.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section17.subtitle")}
-        </span>
         <p className="term__text SFPro-300 display-7">
           {translate("termOfUsePage.section17.text.part1")}
         </p>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section17.text.part2")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section18.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section18.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section18.text.part1")}
         </p>
 
@@ -314,77 +260,56 @@ const TermOfUse = () => {
         <span className="mb-4 d-block SFPro-600 display-7">
           {translate("termOfUsePage.section19.subtitle4")}
         </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section19.text4")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section20.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section20.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section20.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section21.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section21.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section21.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section22.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section22.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section22.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section23.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section23.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section23.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section24.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section24.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section24.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section25.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section25.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section25.text.part1")}
         </p>
 
         <h3 className="display-6 SFPro-600">
           {translate("termOfUsePage.section26.title")}
         </h3>
-        <span className="mb-4 d-block SFPro-300 display-7">
-          {translate("termOfUsePage.section26.subtitle")}
-        </span>
-        <p className="term__text SFPro-300 display-7 mb-7">
+        <p className="term__text SFPro-300 display-7 mb-6">
           {translate("termOfUsePage.section26.text.part1")}
         </p>
 
