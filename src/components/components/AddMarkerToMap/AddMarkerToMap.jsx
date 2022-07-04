4c095480d1a223e6c93d58eed723be7990a7e2bc
Action007 <aqshin.baghirzade@gmail.com>
2022-07-04 10:24:04 +0400

    update

--- src/components/components/AddMarkerToMap/AddMarkerToMap.jsx
+++ src/components/components/AddMarkerToMap/AddMarkerToMap.jsx
@@ -1,31 +1,29 @@
-import React from "react";
-import Leaflet from "leaflet";
-import { Marker, useMapEvents } from "react-leaflet";
-import { useDispatch } from "react-redux";
-import { createChecklistActions } from "../../../store/createChecklistSlice";
+import React from 'react'
+import Leaflet from 'leaflet'
+import { Marker, useMapEvents } from 'react-leaflet'
+import { useDispatch } from 'react-redux'
+import { createChecklistActions } from '../../../store/createChecklistSlice'
 
-import MapImg from "../../../assets/images/icon/map-marker.png";
+import MapImg from '../../../assets/images/icon/map-marker.png'
 
 const AddMarkerToMap = ({ coordinates, id, creation }) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   const myIcon = Leaflet.icon({
     iconUrl: MapImg,
     iconSize: [21, 25], // size of the icon
     iconAnchor: [10, 26], // point of the icon which will correspond to marker's location
-  });
+  })
 
   const map = useMapEvents({
     click(e) {
-      if (!creation) return;
-      const { lat, lng } = e.latlng;
-      map.flyTo(e.latlng, map.getZoom());
-      dispatch(
-        createChecklistActions.addCoordinate({ id, latLng: { lat, lon: lng } })
-      );
+      if (!creation) return
+      const { lat, lng } = e.latlng
+      map.flyTo(e.latlng, map.getZoom())
+      dispatch(createChecklistActions.addCoordinate({ id, latLng: { lat, lon: lng } }))
     },
-  });
+  })
 
-  return coordinates ? <Marker position={coordinates} icon={myIcon} /> : null;
-};
+  return coordinates ? <Marker position={coordinates} icon={myIcon} /> : null
+}
 
-export default AddMarkerToMap;
+export default AddMarkerToMap
