2376a4112608ab343c303b71305a61ca198575a7
Aqshin Baghirzade <aqshin.baghirzade@gmail.com>
2022-07-06 12:38:36 +0000

    CCM-297 (breadcrumbs)

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { checklistAPI } from "../services/checklistService";
 import { createChecklistReducer } from "./createChecklistSlice";
+import { homePageFiltersSliceReducer } from "./homePageFiltersSlice";
 import { authSliceReducer } from "./authSlice";
 
 const store = configureStore({
@@ -8,6 +9,7 @@ const store = configureStore({
     [checklistAPI.reducerPath]: checklistAPI.reducer,
     createChecklistReducer,
     authSliceReducer,
+    homePageFiltersSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(checklistAPI.middleware),
